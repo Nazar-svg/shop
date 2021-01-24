c3d5ed242f5d2eeb4a4e325d244bf1b4ae647d3a
Nazar-svg <nazar.kolos90@gmail.com>
2021-01-24 15:36:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Layout from './hoc/Layout/Layout';
+import Auth from './containers/Auth/Auth';
+import ProductList from './components/ProductList/ProductList';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/productList" component={ProductList} />
+        <Route path="/" component={Auth} />
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './ProductList.module.css'
+
+const ProductList = props => {
+   return(
+       <div className={classes.ProductList}>
+           <h1>Список</h1>
+       </div>
+   )
+}
+
+export default ProductList;
